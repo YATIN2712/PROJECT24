9c338aa2a6460172a27feb5da77d53369f0c99da
YAIN2712 <yatindixit1989@oulook.com>
2024-02-09 13:54:27 +0530

    updated and created files

--- Module2.py
+++ Module2.py
@@ -9,9 +9,9 @@ import os
 import ply.lex as lex
 import ply.yacc as yacc
 from urllib.request import Request, urlopen
-req = Request('https://en.wikipedia.org/wiki/Responses_to_the_COVID-19_pandemic_in_February_2020',headers ={'User-Agent':'Google/1.0'})
+req = Request('https://en.wikipedia.org/wiki/Timeline_of_the_COVID-19_pandemic_in_April_2020',headers ={'User-Agent':'Google/1.0'})
 webpage = urlopen(req).read()
 mydata = webpage.decode("utf8")
-f=open('feb2020.html','w',encoding="utf-8")
+f=open('apr2020.html','w',encoding="utf-8")
 f.write(mydata)
 f.close
