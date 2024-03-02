7d4c0879acbf34ebe4a66bddb21fb9e85719881c
YAIN2712 <yatindixit1989@oulook.com>
2024-03-02 16:54:38 +0530

    updated files

---
+++ Module2.8.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  9 14:51:29 2024
+
+@author: yatin
+"""
+
+# -*- coding: utf-8 -*-
+
+import ply.lex as lex
+import ply.yacc as yacc
+text = ""
+
+###DEFINING TOKENS###
+tokens = ('BEGINTABLE',
+          'OPENTABLE', 'CLOSETABLE', 'OPENROW', 'CLOSEROW',
+          'OPENHEADER', 'CLOSEHEADER', 'OPENHREF', 'CLOSEHREF',
+          'CONTENT', 'OPENDATA', 'CLOSEDATA', 'OPENSPAN',
+          'CLOSESPAN', 'OPENDIV', 'CLOSEDIV', 'OPENSTYLE', 'CLOSESTYLE', 'GARBAGE', 'OPENP', 'CLOSEP', 'END')
+t_ignore = '\t'
+
+###############Tokenizer Rules################
+
+
+def t_BEGINTABLE(t):
+    r'<h3><span.class="mw-headline".id="1_August">1.August</span></h3>'
+    return t
+
+
+def t_END(t):
+    r'<h2><span.class="mw-headline".id="Summary">Summary</span></h2>'
+    return t
+
+
+def t_OPENTABLE(t):
+    r'<tbody[^>]*>'
+    return t
+
+
+def t_CLOSETABLE(t):
+    r'</tbody[^>]*>'
+    return t
+
+
+def t_OPENP(t):
+    r'<p[^>]*>'
+    return t
+
+
+def t_CLOSEP(t):
+    r'</p[^>]*>'
+    return t
+
+
+def t_OPENROW(t):
+    r'<tr[^>]*>'
+    return t
+
+
+def t_CLOSEROW(t):
+    r'</tr[^>]*>'
+    return t
+
+
+def t_OPENHEADER(t):
+    r'<th[^>]*>'
+    return t
+
+
+def t_CLOSEHEADER(t):
+    r'</th[^>]*>'
+    return t
+
+
+def t_OPENHREF(t):
+    r'<a[^>]*>'
+    return t
+
+
+def t_CLOSEHREF(t):
+    r'</a[^>]*>'
+    return t
+
+
+def t_OPENDATA(t):
+    r'<td[^>]*>'
+    return t
+
+
+def t_CLOSEDATA(t):
+    r'</td[^>]*>'
+    return t
+
+
+def t_CONTENT(t):
+    r'[A-Za-z0-9,]+'
+    return t
+
+
+def t_OPENDIV(t):
+    r'<div[^>]*>'
+
+
+def t_CLOSEDIV(t):
+    r'</div[^>]*>'
+
+
+def t_OPENSTYLE(t):
+    r'<style[^>]*>'
+
+
+def t_CLOSESTYLE(t):
+    r'</style[^>]*>'
+
+
+def t_OPENSPAN(t):
+    r'<span[^>]*>'
+
+
+def t_CLOSESPAN(t):
+    r'</span[^>]*>'
+
+
+def t_GARBAGE(t):
+    r'<[^>]*>'
+
+
+def t_error(t):
+    t.lexer.skip(1)
+####################################################################################################################################################################################################
+    # GRAMMAR RULES
+
+
+def p_start(p):
+    '''start : table'''
+    p[0] = p[1]
+
+
+def p_skiptag(p):
+    '''skiptag : CONTENT skiptag empty
+               | OPENHREF skiptag
+               | CLOSEHREF skiptag
+               | OPENP skiptag
+               | CLOSEP skiptag
+               | empty'''
+    global text
+    if len(p) == 4:
+        text = store(p[1])
+
+
+def p_table(p):
+    '''table : BEGINTABLE skiptag END'''
+
+
+def p_empty(p):
+    '''empty :'''
+    pass
+
+
+def p_content(p):
+    '''content : CONTENT
+               | empty'''
+    p[0] = p[1]
+
+
+def p_error(p):
+    pass
+
+#########DRIVER FUNCTION#######
+
+
+def store(text1):
+    global text
+    text1 += ' ' + text
+    return text1
+
+
+def main():
+    file_obj = open('aug2020.html', 'r', encoding="utf-8")
+    data = file_obj.read()
+    lexer = lex.lex()
+    lexer.input(data)
+    for tok in lexer:
+        print(tok)
+    parser = yacc.yacc()
+
+    def extract_data(parser):
+
+        result = parser.parse(data)
+        with open('aug2020.txt', 'w') as file:
+            file.write(text)
+    # Menu loop
+    while True:
+        print("\nMenu:")
+        print("1. Extract Data")
+        print("2. Quit")
+
+        choice = input("Enter your choice (1 or 2): ")
+
+        if choice == '1':
+            extract_data(parser)
+        elif choice == '2':
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+
+    file_obj.close()
+
+
+if __name__ == '__main__':
+    main()
--- Module2.py
+++ Module2.py
@@ -9,9 +9,9 @@ import os
 import ply.lex as lex
 import ply.yacc as yacc
 from urllib.request import Request, urlopen
-req = Request('https://en.wikipedia.org/wiki/Timeline_of_the_COVID-19_pandemic_in_June_2020',headers ={'User-Agent':'Google/1.0'})
+req = Request('https://en.wikipedia.org/wiki/Timeline_of_the_COVID-19_pandemic_in_December_2020',headers ={'User-Agent':'Google/1.0'})
 webpage = urlopen(req).read()
 mydata = webpage.decode("utf8")
-f=open('jun2020.html','w',encoding="utf-8")
+f=open('dec2020.html','w',encoding="utf-8")
 f.write(mydata)
 f.close
